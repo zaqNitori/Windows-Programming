253ef310410d026c5b434edc4b76afe66a446c1b
zaqNitori <zaq81009@gmail.com>
2021-10-02 18:56:32 +0800

    新增 PresentationModel，用來處理View的資料跟邏輯

---
+++ homework/PresentationModel/CoursePresentationModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework.PresentationModel
+{
+    public class CoursePresentationModel
+    {
+        private bool _isButtonSendEnable = false;
+        private bool _isButtonShowSelectResultEnable = true;
+
+        public CoursePresentationModel()
+        {
+
+        }
+
+        /// <summary>
+        /// Get Course Data from HTMLParser
+        /// </summary>
+        /// <returns>Course Data</returns>
+        /// <history>
+        ///     1.  2021.10.02  create function
+        /// </history>
+        public List<ViewModel.Course> GetCourse(string url)
+        {
+            return HTMLParser.GetCourse(url);
+        }
+
+        /// <summary>
+        /// btnSend status
+        /// </summary>
+        public bool IsButtonSendEnable()
+        {
+            return _isButtonSendEnable;
+        }
+
+        /// <summary>
+        /// btnShowSelectResult status
+        /// </summary>
+        public bool IsButtonShowSelectResultEnable()
+        {
+            return _isButtonShowSelectResultEnable;
+        }
+
+    }
+}
