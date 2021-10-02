cf0ccfe27e58453509b932e78e93ee42f05a1621
zaqNitori <zaq81009@gmail.com>
2021-10-02 18:57:34 +0800

    程式開始Run的位置 program.cs

--- homework/Program.cs
+++ homework/Program.cs
@@ -14,10 +14,9 @@ namespace homework
         [STAThread]
         static void Main()
         {
-            Model model = new Model();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MyClassForm(model));
+            Application.Run(new SelectCourseForm(new PresentationModel.CoursePresentationModel()));
         }
     }
 }
