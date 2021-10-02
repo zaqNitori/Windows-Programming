714b3fac9dd4aef9b89fff24b07f98e6078035ec
zaqNitori <zaq81009@gmail.com>
2021-10-02 18:56:53 +0800

    修改Model存放位置

---
+++ homework/Model/Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace homework.Model
+{
+    public class Model
+    {
+        public Model()
+        {
+
+        }
+
+        /// <summary>
+        /// 取得畫面上選取到的資料內容
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.02  create function
+        /// </history>
+        public string GetSelectedData(DataGridViewRowCollection dataSource)
+        {
+            string message = string.Empty;
+            foreach (DataGridViewRow row in dataSource)
+            {
+                if (Convert.ToBoolean(row.Cells["checkBoxCol"].Value) == true)
+                {
+                    message += Convert.ToString(row.Cells[ViewModel.CourseProperty.Number.ToString()].Value) 
+                        + Convert.ToString(row.Cells[ViewModel.CourseProperty.Name.ToString()].Value) 
+                        + Environment.NewLine;
+                }
+            }
+            return message;
+        }
+
+    }
+}
