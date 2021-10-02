3cd984b83b4f2d2b06fea1d29300a62fdf53da16
zaqNitori <zaq81009@gmail.com>
2021-10-02 18:55:28 +0800

    Form 改名為 SelectCourseForm

---
+++ homework/SelectCourseForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace homework
+{
+    partial class SelectCourseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.courseGridView = new System.Windows.Forms.DataGridView();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonShowSelectResult = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // courseGridView
+            // 
+            this.courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.courseGridView.Location = new System.Drawing.Point(12, 12);
+            this.courseGridView.Name = "courseGridView";
+            this.courseGridView.RowHeadersWidth = 51;
+            this.courseGridView.RowTemplate.Height = 27;
+            this.courseGridView.Size = new System.Drawing.Size(1047, 432);
+            this.courseGridView.TabIndex = 0;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.AutoSize = true;
+            this.buttonSend.Location = new System.Drawing.Point(660, 451);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(122, 56);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "確認送出";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // buttonShowSelectResult
+            // 
+            this.buttonShowSelectResult.AutoSize = true;
+            this.buttonShowSelectResult.Location = new System.Drawing.Point(855, 451);
+            this.buttonShowSelectResult.Name = "buttonShowSelectResult";
+            this.buttonShowSelectResult.Size = new System.Drawing.Size(120, 55);
+            this.buttonShowSelectResult.TabIndex = 2;
+            this.buttonShowSelectResult.Text = "顯示選課結果";
+            this.buttonShowSelectResult.UseVisualStyleBackColor = true;
+            // 
+            // SelectCourseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 518);
+            this.Controls.Add(this.buttonShowSelectResult);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.courseGridView);
+            this.Name = "SelectCourseForm";
+            this.Text = "SelectCourseForm";
+            this.Load += new System.EventHandler(this.SelectCourseFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView courseGridView;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonShowSelectResult;
+    }
+}
---
+++ homework/SelectCourseForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework
+{
+    public partial class SelectCourseForm : Form
+    {
+        private PresentationModel.CoursePresentationModel _coursePresentationModel;
+        private string _courseUrl = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-4&year=110&sem=1&code=2433";
+
+        public SelectCourseForm(PresentationModel.CoursePresentationModel coursePresentationModel)
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            _coursePresentationModel = coursePresentationModel;
+        }
+
+        /// <summary>
+        /// Form Load Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <history>
+        ///     1.  2021.10.02  create function 
+        /// </history>
+        private void SelectCourseFormLoad(object sender, EventArgs e)
+        {
+            RefreshFormObject();
+            BindCourseData();
+        }
+
+        /// <summary>
+        /// Initialize form object
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.02  create function
+        /// </history>
+        private void RefreshFormObject()
+        {
+            buttonSend.Enabled = _coursePresentationModel.IsButtonSendEnable();
+            buttonShowSelectResult.Enabled = _coursePresentationModel.IsButtonShowSelectResultEnable();
+        }
+
+        /// <summary>
+        /// 初始化dataGridView設定
+        /// </summary>
+        /// <history>
+        ///     1.  2021.09.30  create function
+        /// </history>
+        private void InitializeDataGridView()
+        {
+            courseGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            AddCheckBox();
+            courseGridView.RowHeadersVisible = false;
+            courseGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            //courseGridView.CellContentClick += courseGridView_CheckBoxCheck;
+        }
+
+        /// <summary>
+        /// 加入CheckBox控制項
+        /// </summary>
+        /// <history>
+        ///     1.  2021.09.30  create function
+        /// </history>
+        private void AddCheckBox()
+        {
+            DataGridViewColumn columnCheck = new DataGridViewCheckBoxColumn();
+            columnCheck.Width = 45;
+            columnCheck.Name = "checkBoxCol";
+            courseGridView.Columns.Insert(0, columnCheck);
+            courseGridView.Columns[0].HeaderText = "選取";
+        }
+
+        /// <summary>
+        /// Bind Course Grid View DataSource
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.02  create function
+        /// </history>
+        private void BindCourseData()
+        {
+            courseGridView.DataSource = _coursePresentationModel.GetCourse(_courseUrl);
+        }
+
+        /// <summary>
+        /// dataGridView 內 CheckBox被點擊的事件
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.02  create function
+        /// </history>
+        private void dataGridView1_CheckBoxCheck(object sender, EventArgs e)
+        {
+            //MessageBox.Show(dataGridView1.CurrentCell.RowIndex.ToString());
+        }
+
+        /// <summary>
+        /// 顯示選課結果button 點擊事件
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.02  create function
+        /// </history>
+        private void buttonCheckSelect_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(_model.GetSelectedData(dataGridView1.Rows));
+            //MessageBox.Show(Convert.ToString(dataGridView1.Rows[3].Cells["Number"].Value));
+        }
+
+    }
+}
