a21e5a51fb8318bddeb503ceb7e50dd0816ebe8a
zaqNitori <zaq81009@gmail.com>
2021-10-02 22:59:24 +0800

    presentationModel 新增 儲存點選的rowIndex，以及 顯示選課結果

--- homework/PresentationModel/CoursePresentationModel.cs
+++ homework/PresentationModel/CoursePresentationModel.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace homework.PresentationModel
 {
@@ -10,14 +9,18 @@ namespace homework.PresentationModel
     {
         private bool _isButtonSendEnable = false;
         private bool _isButtonShowSelectResultEnable = true;
+        private const string TARGET = "checkBoxCol";
+        private Model.Model _model;
+        private HashSet<int> _courseSelectData;
 
-        public CoursePresentationModel()
+        public CoursePresentationModel(Model.Model model)
         {
-
+            _model = model;
+            _courseSelectData = new HashSet<int>();
         }
 
         /// <summary>
-        /// Get Course Data from HTMLParser
+        /// Get Course Data 
         /// </summary>
         /// <returns>Course Data</returns>
         /// <history>
@@ -25,7 +28,7 @@ namespace homework.PresentationModel
         /// </history>
         public List<ViewModel.Course> GetCourse(string url)
         {
-            return HTMLParser.GetCourse(url);
+            return _model.GetCourse(url);
         }
 
         /// <summary>
@@ -42,6 +45,55 @@ namespace homework.PresentationModel
         public bool IsButtonShowSelectResultEnable()
         {
             return _isButtonShowSelectResultEnable;
+        }
+
+        /// <summary>
+        /// 紀錄當前畫面上checkbox的狀態
+        /// </summary>
+        /// <param name="index"></param>
+        public void SetCourseCheckBoxStatus(int index)
+        {
+            if (_courseSelectData.Contains(index))
+            {
+                _courseSelectData.Remove(index);
+            }
+            else
+            {
+                _courseSelectData.Add(index);
+            }
+
+            if (_courseSelectData.Count == 0)
+            {
+                _isButtonSendEnable = false;
+            }
+            else
+            {
+                _isButtonSendEnable = true;
+            }
+        }
+
+        /// <summary>
+        /// 取得畫面上選取到的資料內容
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.02  create function
+        /// </history>
+        public string GetSelectedResult(DataGridViewRowCollection dataSource)
+        {
+            string message = string.Empty;
+            List<int> selectIndex;
+            
+            if (_courseSelectData.Count > 0)
+            {
+                selectIndex = _courseSelectData.ToList();
+                foreach (int i in selectIndex)
+                {
+                    message += Convert.ToString(dataSource[i].Cells[ViewModel.CourseProperty.Number.ToString()].Value)
+                        + Convert.ToString(dataSource[i].Cells[ViewModel.CourseProperty.Name.ToString()].Value)
+                        + Environment.NewLine;
+                }
+            }
+            return message;
         }
 
     }
