9ec79b13e94d74748266f8e609fb86f8db7eb019
zaqNitori <zaq81009@gmail.com>
2021-10-10 22:53:03 +0800

    修改program.cs  讓程式預設開啟StartUpForm，再藉由此視窗做接下來的動作。

--- homework/Program.cs
+++ homework/Program.cs
@@ -14,11 +14,9 @@ namespace homework
         [STAThread]
         static void Main()
         {
-            string courseUrl = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-4&year=110&sem=1&code=2433";
-            Model.Model model = new Model.Model(new Model.CourseCrawler(courseUrl));
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SelectCourseForm(new PresentationModel.CoursePresentationModel(model)));
+            Application.Run(new StartUpForm());
         }
     }
 }
