e4a9cdeef3ebf1be1d045023ff4c098bf36931b9
zaqNitori <zaq81009@gmail.com>
2021-10-10 22:54:54 +0800

    修改 物件名稱 使其符合DR.Smell 的規則

--- homework/ManageCourseForm.Designer.cs
+++ homework/ManageCourseForm.Designer.cs
@@ -29,26 +29,26 @@ namespace homework
         /// </summary>
         private void InitializeComponent()
         {
-            this._CommingSoonLabel = new System.Windows.Forms.Label();
+            this._alertText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // _CommingSoonLabel
+            // _alertText
             // 
-            this._CommingSoonLabel.AutoSize = true;
-            this._CommingSoonLabel.Font = new System.Drawing.Font("新細明體", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this._CommingSoonLabel.Location = new System.Drawing.Point(12, 157);
-            this._CommingSoonLabel.Name = "_CommingSoonLabel";
-            this._CommingSoonLabel.Size = new System.Drawing.Size(762, 120);
-            this._CommingSoonLabel.TabIndex = 0;
-            this._CommingSoonLabel.Text = "Comming Soon";
-            this._CommingSoonLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this._alertText.AutoSize = true;
+            this._alertText.Font = new System.Drawing.Font("新細明體", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this._alertText.Location = new System.Drawing.Point(12, 157);
+            this._alertText.Name = "_alertText";
+            this._alertText.Size = new System.Drawing.Size(762, 120);
+            this._alertText.TabIndex = 0;
+            this._alertText.Text = "Comming Soon";
+            this._alertText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ManageCourseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this._CommingSoonLabel);
+            this.Controls.Add(this._alertText);
             this.Name = "ManageCourseForm";
             this.Text = "ManageCourseForm";
             this.ResumeLayout(false);
@@ -58,6 +58,6 @@ namespace homework
 
         #endregion
 
-        private System.Windows.Forms.Label _CommingSoonLabel;
+        private System.Windows.Forms.Label _alertText;
     }
 }
