b64630a612a2ccbd9b5386d34fd9b7a58d96f7d1
zaqNitori <zaq81009@gmail.com>
2021-10-16 23:00:12 +0800

    修改form的位置

--- homework/MyForm/StartUpForm.cs
+++ homework/MyForm/StartUpForm.cs
@@ -12,12 +12,16 @@ namespace homework
 {
     public partial class StartUpForm : Form
     {
-        public StartUpForm()
+        private PresentationModel.StartUpPresentationModel _startUpPresentationModel;
+
+        public StartUpForm(PresentationModel.StartUpPresentationModel startUpPresentationModel)
         {
             InitializeComponent();
+            RefreshWidgetStatus();
             _buttonExit.Click += ExitStartUpForm;
             _buttonCourseSelecting.Click += OpenSelectCourseForm;
             _buttonCourseManagement.Click += OpenManageCourseForm;
+            _startUpPresentationModel = startUpPresentationModel;
         }
 
         /// <summary>
@@ -45,7 +49,7 @@ namespace homework
         {
             string courseUrl = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-4&year=110&sem=1&code=2433";
             Model.Model model = new Model.Model(new Model.CourseCrawler(courseUrl));
-            SelectCourseForm selectCourse = new SelectCourseForm(new PresentationModel.CoursePresentationModel(model));
+            SelectCourseForm selectCourse = new SelectCourseForm(new PresentationModel.CourseSelectingPresentationModel(model));
             selectCourse.Show();
         }
 
@@ -61,6 +65,19 @@ namespace homework
         {
             ManageCourseForm manageCourse = new ManageCourseForm();
             manageCourse.Show();
+        }
+
+        /// <summary>
+        /// Initialize form status
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.16  create function
+        /// </history>
+        private void RefreshWidgetStatus()
+        {
+            _buttonExit.Enabled = _startUpPresentationModel.IsButtonExitEnabled();
+            _buttonCourseSelecting.Enabled = _startUpPresentationModel.IsButtonCourseSelectingEnabled();
+            _buttonCourseManagement.Enabled = _startUpPresentationModel.IsButtonCourseManagementEnabled();
         }
 
     }
