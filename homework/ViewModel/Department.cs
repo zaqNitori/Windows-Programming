b505493073cf2f31555efe7bc0193b11e273e7ad
zaqNitori <zaq81009@gmail.com>
2021-10-25 01:20:38 +0800

    新增 ViewModel  Department 以及 Curriculum

---
+++ homework/ViewModel/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework.ViewModel
+{
+    public class Department
+    {
+        public Department(string name)
+        {
+            DepartmentName = name;
+        }
+
+        public string DepartmentName
+        {
+            get; set;
+        }
+    }
+}
