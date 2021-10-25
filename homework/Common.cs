569e34ae312cd0a6eeda8a9abc08699dd47465e5
zaqNitori <zaq81009@gmail.com>
2021-10-25 18:42:48 +0800

    新增共用 class， 目前功能為 開啟datagridview的DoubleBuffered功能

---
+++ homework/Common.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework
+{
+    public class Common
+    {
+        /// <summary>
+        /// 啟用datagridview的doublebuffered
+        /// </summary>
+        /// <history>
+        ///     1.  2021.10.25  create function
+        /// </history>
+        public static void ActivateDoubleBuffer(DataGridView dataGridView)
+        {
+            const string DOUBLE_BUFFERED = "DoubleBuffered";
+
+            Type type = dataGridView.GetType();
+            PropertyInfo propertyInfo = type.GetProperty(DOUBLE_BUFFERED, BindingFlags.Instance | BindingFlags.NonPublic);
+            propertyInfo.SetValue(dataGridView, true, null);
+        }
+    }
+}
