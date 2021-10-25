c46d23a4dc81d2304ab3cdedae5a2cae42cf4e29
zaqNitori <zaq81009@gmail.com>
2021-10-25 18:47:01 +0800

    其餘檔案

--- homework/Program.cs
+++ homework/Program.cs
@@ -10,6 +10,7 @@ namespace homework
 {
     static class Program
     {
+
         /// <summary>
         /// 應用程式的主要進入點。
         /// </summary>
@@ -18,20 +19,19 @@ namespace homework
         {
             const string COURSE1 = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-4&year=110&sem=1&code=2433";
             const string COURSE2 = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-4&year=109&sem=1&code=2433";
+            List<string> courseUrl = new List<string>();
+            courseUrl.Add(COURSE1);
+            courseUrl.Add(COURSE2);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
             CourseModel courseModel = new CourseModel();
-
+            courseModel.FetchCourseInfo(courseUrl);
             CourseSelectingPresentationModel courseSelectingPresentationModel = new CourseSelectingPresentationModel(courseModel);
+            CourseSelectResultPresentationModel courseSelectingResultPresentationModel = new CourseSelectResultPresentationModel(courseModel);
             StartUpPresentationModel startUpPresentationModel = new StartUpPresentationModel(courseModel);
-            startUpPresentationModel.AddCourseUrl(COURSE1);
-            startUpPresentationModel.AddCourseUrl(COURSE2);
-            startUpPresentationModel.FetchCourse();
-
             StartUpForm startUpForm = new StartUpForm(startUpPresentationModel);
             startUpForm.SetCourseSelectingPresentationModel(courseSelectingPresentationModel);
-
+            startUpForm.SetCourseSelectingResultPresentationModel(courseSelectingResultPresentationModel);
             Application.Run(startUpForm);
         }
     }
