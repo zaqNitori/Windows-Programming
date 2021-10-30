44d481937bdc8764df55a718c22490fade93d9f2
zaqNitori <zaq81009@gmail.com>
2021-10-30 18:23:48 +0800

    新增 CourseManagementPM

---
+++ homework/PresentationModel/CourseManagementPresentationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using homework.Model;
+
+namespace homework.PresentationModel
+{
+    public class CourseManagementPresentationModel
+    {
+        private CourseModel _courseModel;
+
+        public CourseManagementPresentationModel(CourseModel courseModel)
+        {
+            _courseModel = courseModel;
+        }
+    }
+}
