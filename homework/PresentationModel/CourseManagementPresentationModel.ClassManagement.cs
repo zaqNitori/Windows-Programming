169a53fc0ad75b84ddd529d38b9f124d48f23829
zaqNitori <zaq81009@gmail.com>
2021-11-27 21:27:07 +0800

    完成班級管理畫面

---
+++ homework/PresentationModel/CourseManagementPresentationModel.ClassManagement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using homework.Manager;
+using homework.ViewModel;
+
+namespace homework.PresentationModel
+{
+    partial class CourseManagementPresentationModel
+    {
+        /// <summary>
+        /// 取得所有課程名稱
+        /// </summary>
+        public List<DataItem> GetAllClassName()
+        {
+            return _courseManageModel.GetClassNameAsItem();
+        }
+    }
+}
